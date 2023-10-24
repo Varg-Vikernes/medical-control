c25b29d20c1d267cd4b4cf2e892848f41697a9e4
Varg-Vikernes1~ <vikernesvarg058@gmail.com>
2023-10-24 09:19:35 -0600

    Modulo identificacion en proceso de aprobacion

--- src/components/Hcx1/FichaIdentificacion/identificacion.jsx
+++ src/components/Hcx1/FichaIdentificacion/identificacion.jsx
@@ -6,7 +6,15 @@ const Identificacion = () => {
     const [sexo, setSexo] = useState('');
     const [estadoCivil, setEstadoCivil] = useState('');
     const [turno, setTurno] = useState('');
-
+    const [lugFecha, setLugFecha] = useState('');
+    const [planta, setPlanta] = useState('');
+    const [tipoContrato, setTipoContrato] = useState('');
+    const [dpto, setDpto] = useState('');
+    const [nombreSup, setNombreSup] = useState('');
+    const [antiguedadPlanta, setAntiguedadPlanta] = useState('');
+    const [antiguedadPuesto, setAntiguedadPuesto] = useState('');
+    const [puestTrabajo, setPuestoTrabajo] = useState('');
+    const [domicilio, setDomicilio] = useState('');
     return (
         <form action="" className="w-full p-4">
             <h1 className="text-xl  font-sans mb-4">Ficha de identificacion</h1>
@@ -59,6 +67,95 @@ const Identificacion = () => {
                     </div>
                 </div>
             </div >
+            <div className="flex-1 mr-4">
+                <label htmlFor="" className="block font-bold">
+                    Lugar y fecha de nacimiento:
+                </label>
+                <input type="text" id="lugar" name="lugar"
+                    className="w-full border p-2 w-full"
+                    value={lugFecha}
+                    onChange={(e) => setLugFecha(e.target.value)}
+                />
+            </div>
+            <div className="flex justify-between mb-4">
+                <div className="flex-1 mr-4">
+                    <label htmlFor="planta" className="block font-bold">
+                        Planta:
+                    </label>
+                    <input type="text" name="planta" id="planta"
+                        className="w-full border p-2" value={planta}
+                        onChange={(e) => setPlanta(e.target.value)}
+                    />
+                </div>
+                <div className="flex-1 mr-4">
+                    <label htmlFor="tipoContrato" className="block font-bold">
+                        Tipo de contrato:
+                    </label>
+                    <input type="text" name="contrato" id="contrato"
+                        className="w-full border p-2 " value={tipoContrato}
+                        onChange={(e) => setTipoContrato(e.target.value)}
+                    />
+                </div>
+                <div className="flex-1 mr-4">
+                    <label htmlFor="dpto" className="block font-bold">
+                        Departamento:
+                    </label>
+                    <input type="text" name="dpto" id="dpto"
+                        className="w-full border p-2" value={dpto}
+                        onChange={(e) => setDpto(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="flex justify-between mb-4">
+                <div className="flex-1 mr-4">
+                    <label htmlFor="NomnbreSupervisor" className="block font-bold">
+                        Nombre del supervisor:
+                    </label>
+                    <input type="text" name="NombreSupervisor" id="NombreSupervisor"
+                        className="w-full border p-2" value={nombreSup}
+                        onChange={(e) => setNombreSup(e.target.value)}
+                    />
+                </div>
+                <div className="flex-1 mr-4">
+                    <label htmlFor="antiguedadPlanta" className="block font-bold">
+                        Antiguedad de planta
+                    </label>
+                    <input type="text" name="antiguedadPlanta" id="antiguedadPlanta"
+                        className="w-full border p-2" value={antiguedadPlanta}
+                        onChange={(e) => setAntiguedadPlanta(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="flex justify-between mb-4">
+                <div className="flex-1 mr-4">
+                    <label htmlFor="antiguedadPuesto" className="block font-bold">
+                        Antiguedad del puesto:
+                    </label>
+                    <input type="text" name="antiguedadPuesto" id="antiguedadPuesto"
+                        className="w-full border p-2" value={antiguedadPuesto}
+                        onChange={(e) => setAntiguedadPuesto(e.target.value)}
+                    />
+                </div>
+                <div className="flex-1 mr-4">
+                    <label htmlFor="puestoTrabajo" className="block font-bold">
+                        Puesto del trabajo:
+                    </label>
+                    <input type="text" name="puestoTrabajo" id="puestoTrabajo"
+                        className="w-full border p-2" value={puestTrabajo}
+                        onChange={(e) => setPuestoTrabajo(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="flex-1 mr-4">
+                <label htmlFor="domicilioActual" className="block font-bold">
+                    Domicilio actual:
+                </label>
+                <input type="text" id="domicilioActual" name="domicilioActual"
+                    className="w-full border p-2 w-full"
+                    value={domicilio}
+                    onChange={(e) => setDomicilio(e.target.value)}
+                />
+            </div>
         </form >
     );
 }
