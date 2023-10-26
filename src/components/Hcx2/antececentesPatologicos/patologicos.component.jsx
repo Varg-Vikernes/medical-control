9582f0efbbb158703345180ef6e4436fe1e2b2fe
Varg-Vikernes1~ <vikernesvarg058@gmail.com>
2023-10-26 08:43:22 -0600

    Tabla centro

--- src/components/Hcx2/antececentesPatologicos/patologicos.component.jsx
+++ src/components/Hcx2/antececentesPatologicos/patologicos.component.jsx
@@ -9,23 +9,71 @@ const AntPatologico = () => {
             <div className="flex justify-betwen mb-4">
                 <div className="flex-1 mr-4">
                     {/* Tabla de lado Izquierdo */}
-                    <table className="table-full border">
+                    <table className="table-auto  mx-auto my-4">
                         <tbody>
                             <tr>
-                                <td className="border p-2 w-40">Alergico</td>
-                                <td className="border p-2 w-20">
+                                <td className="border p-2 w-40 text-center">Alergico</td>
+                                <td className="border p-2 w-20 text-center">
                                     <input type="checkbox" />
                                 </td>
                             </tr>
                             <tr>
-                                <td className="border p-2">Diabetico</td>
-                                <td className="border p-2">
+                                <td className="border p-2 w-40 text-center">Diabetico</td>
+                                <td className="border p-2 w-40 text-center">
                                     <input type="checkbox" />
                                 </td>
                             </tr>
                             <tr>
-                                <td className="border p-2">Diabetico</td>
-                                <td className="border p-2">
+                                <td className="border p-2 w-40 text-center">Diabetico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Artritico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Cardiovascular</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Hipertenso</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Hematologico</td>
+                                <td className="border p-2 w-40 text-center" >
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Traumatico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Siquiatrico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Neoplastico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Quirurgico</td>
+                                <td className="border p-2 w-40 text-center">
                                     <input type="checkbox" />
                                 </td>
                             </tr>
@@ -34,12 +82,71 @@ const AntPatologico = () => {
                 </div>
                 <div className="flex-1 mr-4">
                     {/* Tabla de centro */}
-                    <table className="table-auto">
+                    <table className="table-auto  mx-auto my-4">
                         <tbody>
                             <tr>
-                                <td className="border o-2">Endocrinologo</td>
-                                <td className="border p-2">
-                                    <input type="checkbox" />                        </td>
+                                <td className="border p-2 w-40 text-center">Endocrinologo</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Genitourinario</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Neurologico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Asmatico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Icterico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Congenito</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Audiologico</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Transfuncional</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Parasitos</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center">Otros</td>
+                                <td className="border p-2 w-40 text-center">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border p-2 w-40 text-center"></td>
+                                <td className="border p-2 w-40 text-center"></td>
                             </tr>
                         </tbody>
                     </table>
@@ -47,11 +154,11 @@ const AntPatologico = () => {
 
                 <div className="flex-1 mr-4">
                     {/* Tabla lado derecho */}
-                    <table className="table-auto">
+                    <table className="table-auto  mx-auto my-4">
                         <tbody>
                             <tr>
-                                <td className="border p-2">Sarampion</td>
-                                <td className="border p-2">
+                                <td className="border p-2 w-40 text-center">Sarampion</td>
+                                <td className="border p-2 w-40 text-center">
                                     <input type="checkbox" />
                                 </td>
                             </tr>
