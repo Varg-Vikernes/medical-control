e3b8efdbdcd2b66d304488ea59ee87b13174b867
Varg-Vikernes1~ <vikernesvarg058@gmail.com>
2023-11-03 19:12:46 -0600

    Extremidades superiores

---
+++ src/components/Hcx4/extremidades/superiores.component.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+const ExtremidadesSuperiores = () => {
+    return (
+        <>
+            <form action="" className="w-full p-4">
+                <h1 className="text-xl  font-sans mb-4">Extremidades superiores</h1>
+                <div className="flex justify-between mr-4">
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="flex justify-between mr-4">
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="flex justify-between mr-4">
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex-1 mr-4">
+                        <label htmlFor="" className="block font-bold"></label>
+                        <input type="text" name="" id=""
+                            className="w-full border p-2"
+                            value={ } onChange={(e) => Set(e.target.value)}
+                        />
+                    </div>
+
+                    <label htmlFor="" className="block font-bold"></label>
+                    <input type="text" name="" id=""
+                        className="w-full border p-2"
+                        value={ } onChange={(e) => Set(e.target.value)}
+                    />
+                </div>
+            </form>
+        </>
+    )
+}
+export default ExtremidadesSuperiores;
