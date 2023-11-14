1a6c073c20ab68f15e75baf8a6fddb78e41bca1a
Varg-Vikernes1~ <vikernesvarg058@gmail.com>
2023-11-13 21:54:38 -0600

    sideBar

--- src/components/Hcx2/hcx2.jsx
+++ src/components/Hcx2/hcx2.jsx
@@ -8,28 +8,36 @@ import AntecedentesPorAparatos from "./InterrogatorioPorAparatos/interrogatorio.
 const Hcx2 = () => {
     return (
         <>
-            <Navbar />
-            <div className="container mx-auto p-4">
-                <div className="flex flex-wrap items-center p-10" >
-                    {/* Imagen de la empresa */}
-                    <div className="w-full md:w-1/2">
-                        <img src="https://th.bing.com/th/id/R.0bb24fc69f3eadf890bc973595958d98?rik=4n7nydXfJTfMxw&pid=ImgRaw&r=0"
-                            alt=""
-                            className="w-60 h-24 object-cover font-sans "
-                        />
+            <div className="flex">
+                <div className="w-auto ">
+                    <Navbar className="w-auto" />
+                </div>
+                <div className="flex-1 flex flex-col">
+                    <div className="container mx-auto p-7">
+                        <div className="flex flex-wrap items-center p-10">
+                            {/* Imagen de la empresa */}
+                            <div className="w-full md:w-1/2">
+                                <img
+                                    src="https://th.bing.com/th/id/R.0bb24fc69f3eadf890bc973595958d98?rik=4n7nydXfJTfMxw&pid=ImgRaw&r=0"
+                                    alt=""
+                                    className="w-60 h-24 object-cover font-sans "
+                                />
+                            </div>
+                            {/* Texto que identifica el tipo de formulario*/}
+                            <div className="w-full md:w-1/2 p-4 text-center">
+                                <h1 className="text-4x2 font-bold">
+                                    Historia Clinica --- Servicio Medico
+                                </h1>
+                            </div>
+                        </div>
                     </div>
-                    {/* Texto que identifica el tipo de formulario*/}
-                    <div className="w-full md:w-1/2 p-4 text-center">
-                        <h1 className="text-4x2 font-bold">Historia Clinica ----  Servicio Medico</h1>
-
-                    </div>
+                    {/* Formularios */}
+                    <AntPatologico />
+                    <Obstretico />
+                    <AntececenteMasculino />
+                    <AntecedentesPorAparatos />
                 </div>
             </div>
-            {/* Formularios */}
-            <AntPatologico />
-            <Obstretico />
-            <AntececenteMasculino />
-            <AntecedentesPorAparatos />
         </>
     )
 }
--- src/components/Hcx4/hcx4.jsx
+++ src/components/Hcx4/hcx4.jsx
@@ -11,29 +11,38 @@ import { Diagnostico } from "./Diagnostico/diagnostico.component";
 const Hcx4 = () => {
     return (
         <>
-            <Navbar />
-            <div className="container mx-auto p-4">
-                <div className="flex flex-wrap items-center p-10">
-                    {/* Imagen de la empresa */}
-                    <div className="w-full md:w-1/2">
-                        <img src="https://th.bing.com/th/id/R.0bb24fc69f3eadf890bc973595958d98?rik=4n7nydXfJTfMxw&pid=ImgRaw&r=0"
-                            alt=""
-                            className="w-60 h-24 object-cover font-sans "
-                        />
+            <div className="flex">
+                <div className="w-auto ">
+                    <Navbar className="w-auto" />
+                </div>
+                <div className="flex-1 flex flex-col">
+                    <div className="container mx-auto p-7">
+                        <div className="flex flex-wrap items-center p-10">
+                            {/* Imagen de la empresa */}
+                            <div className="w-full md:w-1/2">
+                                <img
+                                    src="https://th.bing.com/th/id/R.0bb24fc69f3eadf890bc973595958d98?rik=4n7nydXfJTfMxw&pid=ImgRaw&r=0"
+                                    alt=""
+                                    className="w-60 h-24 object-cover font-sans "
+                                />
+                            </div>
+                            {/* Texto que identifica el tipo de formulario*/}
+                            <div className="w-full md:w-1/2 p-4 text-center">
+                                <h1 className="text-4x2 font-bold">
+                                    Historia Clinica --- Servicio Medico
+                                </h1>
+                            </div>
+                        </div>
                     </div>
-                    {/* Texto que identifica el tipo de formulario*/}
-                    <div className="w-full md:w-1/2 p-4 text-center">
-                        <h1 className="text-4x2 font-bold">Historia Clinica ---  Servicio Medico</h1>
-                    </div>
+                    {/* Formularios */}
+                    <Genitales />
+                    <ExtremidadesSuperiores />
+                    <ExtremidadesInferiores />
+                    <ExploracionPsiconeurologica />
+                    <Laboratorio />
+                    <Diagnostico />
                 </div>
             </div>
-            {/* Formularios */}
-            <Genitales />
-            <ExtremidadesSuperiores />
-            <ExtremidadesInferiores />
-            <ExploracionPsiconeurologica />
-            <Laboratorio />
-            <Diagnostico />
         </>
     )
 }
